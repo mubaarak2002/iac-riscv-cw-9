b3cef83d0513c8935f311a70af524b420cdb6c68
mubaarak2002 <106095474+mubaarak2002@users.noreply.github.com>
2022-12-05 20:12:33 +0000

    Update jump_calc.sv

--- jump_calc.sv
+++ jump_calc.sv
@@ -1,11 +1,12 @@
 module jump_calc #(
-    parameter   WIDTH = 32
+    parameter   DATA_WIDTH = 32,
+    parameter   PC_WIDTH = 16
 )(
-    input logic [WIDTH-1:0]  PC,
-    input logic [WIDTH-1:0]  Imm,
-    output logic [WIDTH-1:0] PC_Target
+    input logic [PC_WIDTH-1:0]  PC,
+    input logic [DATA_WIDTH-1:0]  Imm,
+    output logic [PC_WIDTH-1:0] PC_Target
 );
 
-    assign PC_Target = PC + Imm;
+    assign PC_Target = PC + Imm[PC_WIDTH-1:0];
 
 endmodule
