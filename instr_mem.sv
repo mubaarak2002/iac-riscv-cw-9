b4e399050349bbfbf90e668a1ff7829175531861
mubaarak2002 <106095474+mubaarak2002@users.noreply.github.com>
2022-12-06 01:33:56 +0000

    Update instr_mem.sv

--- instr_mem.sv
+++ instr_mem.sv
@@ -6,15 +6,25 @@ module instr_mem #(
     output logic [DATA_WIDTH-1:0]    instr
 );
 
+    logic [7:0]  instrByte1;
+    logic [7:0]  instrByte2;
+    logic [7:0]  instrByte3;
+    logic [7:0]  instrByte4;
 
-logic [DATA_WIDTH-1:0]  rom_array   [2**ADDRESS_WIDTH-1:0];
+
+logic [7:0]  rom_array   [2**ADDRESS_WIDTH-1:0];
+//logic [DATA_WIDTH-1:0]  rom_array   [5:0];
 
 initial begin
         $readmemh("instr.mem", rom_array);
-end;
+end
 
 always_comb begin 
-    instr = rom_array[PC];
+    instrByte1 = rom_array[PC];
+    instrByte2 = rom_array[PC+1];
+    instrByte3 = rom_array[PC+2];
+    instrByte4 = rom_array[PC+3];
+    instr = {instrByte4, instrByte3, instrByte2, instrByte1};
 end
 
 endmodule
