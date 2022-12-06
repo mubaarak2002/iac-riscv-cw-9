45366ca3851aeec6840814d2bb782a140a272904
mubaarak2002 <106095474+mubaarak2002@users.noreply.github.com>
2022-12-06 14:17:03 +0000

    bug fixes

--- ProgramCounter.sv
+++ ProgramCounter.sv
@@ -1,23 +1,27 @@
 module ProgramCounter#(
     //PC width
-    parameter PC_WIDTH = 16,
-    parameter EXTERNAL_PC_WIDTH = 32
+    parameter PC_WIDTH = 16
 )(
     input logic     clk, rst,
     input logic     PCsrc,
-    input logic     [EXTERNAL_PC_WIDTH-1:0] PC_Target,
-    output logic    [EXTERNAL_PC_WIDTH-1:0] PC
+    input logic     [PC_WIDTH-1:0] PC_Target,
+    output logic    [PC_WIDTH-1:0] PC
 );
 
     //Adders. Uses combinational logic.
-    logic [PC_WIDTH-1:0] inc_PC = PC[EXTERNAL_PC_WIDTH/2 - 1:0] + {{(PC_WIDTH-3){1'b0}}, 3'b100};
-    logic [PC_WIDTH-1:0] branch_PC = PC_Target[EXTERNAL_PC_WIDTH/2 - 1:0] + PC[EXTERNAL_PC_WIDTH/2 - 1:0];
+    logic [PC_WIDTH-1:0] inc_PC;
+    logic [PC_WIDTH-1:0] branch_PC;
     logic [PC_WIDTH-1:0] next_PC;
     
     //multiplexer, uses combinational logic. Selects either a branch or PC + 4.
     always_comb begin
     if (PCsrc) next_PC = branch_PC;
     else next_PC = inc_PC;
+    end
+
+    always_comb begin
+        inc_PC = PC + {{(PC_WIDTH-3){1'b0}}, 3'b100};
+        branch_PC = PC + PC_Target;
     end
     
     //PC register. Synchronous with an asynchronous reset
