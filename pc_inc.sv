4fdc413aa1f1aeacb13de5cd78348ebac6f2a6fc
mubaarak2002 <106095474+mubaarak2002@users.noreply.github.com>
2022-12-06 01:10:19 +0000

    bug fixing

--- pc_inc.sv
+++ pc_inc.sv
@@ -1,12 +1,12 @@
 module pc_inc #(
-    paramater WORD_WIDTH = 32
+    parameter WORD_WIDTH = 32
 )(
     input logic  [WORD_WIDTH-1:0] PC,
-    output logic [WORD_WIDTH-1:0] PCnew
+    output logic [WORD_WIDTH-1:0] PC_new
 );
 
     always_comb begin
-        PC_new = PC + 3'b100
+        PC_new = PC + {{(29){1'b0}}, 3'b100};
     end
 
 endmodule
