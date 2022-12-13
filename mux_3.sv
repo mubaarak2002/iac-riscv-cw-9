2d312a0cae717ba0d329504effba416e4144da5d
Yomna Mohamed <yomihamody@gmail.com>
2022-12-13 17:21:37 +0000

    Create mux_3.sv

---
+++ mux_3.sv
@@ -0,0 +1,15 @@
+module mux_3 #(
+    parameter DATA_WIDTH = 32
+)(
+    input logic [DATA_WIDTH-1:0]  option0, option1, option2,
+    input logic [1:0]             sel,
+    output logic [DATA_WIDTH-1:0] dout
+);
+
+    always_comb 
+        case (sel)
+        2'b00: dout = option0;
+        2'b01: dout = option1;
+        2'b10: dout = option2;
+        
+endmodule
