2ff35d4fd64f0f3ba437ed7371a90fa188ccfed0
mubaarak2002 <mubaarak.k@outlook.com>
2022-12-13 17:16:33 +0000

    PC output to Result mux added

--- ProgramCounter.sv
+++ ProgramCounter.sv
@@ -5,20 +5,20 @@ module ProgramCounter#(
     input logic     clk, rst,
     input logic     PCsrc,
     input logic     [PC_WIDTH-1:0] PC_Target,
-    output logic    [PC_WIDTH-1:0] PC
+    output logic    [PC_WIDTH-1:0] PC,
+    output logic    [PC_WIDTH-1:0] inc_PC
 );
 
-    //Adders. Uses combinational logic.
-    logic [PC_WIDTH-1:0] inc_PC;
     logic [PC_WIDTH-1:0] branch_PC;
     logic [PC_WIDTH-1:0] next_PC;
     
-    //multiplexer, uses combinational logic. Selects either a branch or PC + 4.
+    //Adders. Uses combinational logic.
     always_comb begin
         inc_PC = PC + {{(PC_WIDTH-3){1'b0}}, 3'b100};
         branch_PC = PC_Target;
     end
 
+    //multiplexer, uses combinational logic. Selects either a branch or PC + 4.
     always_comb begin
         if (PCsrc) next_PC = branch_PC;
         else next_PC = inc_PC;
